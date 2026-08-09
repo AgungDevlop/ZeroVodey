d4d95591878089d567abff1d2c88fc850bd82755
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-09 12:11:39 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,7 +5,7 @@ export function Download() {
   const videoTitle = sessionStorage.getItem('videoTitle'); // Get video title from session storage
 
 const randomUrls = [
-    'https://wwp.giriucat.com/redirect-zone/5137a848',
+    'https://www.effectivecpmnetwork.com/b3c368xkxz?key=430d12c7fd58ecba841fd905ca2dd3fd',
     'https://dulyhagglermounting.com/2082665'
 ];
 
